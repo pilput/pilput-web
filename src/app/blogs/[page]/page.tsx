27cca34bf7e3206e109e767826336cef09a87088
cecep31 <cecepjanuardi31@gmail.com>
2023-07-25 23:35:07 +0700

    update litle bot blog

--- src/app/blogs/[page]/page.tsx
+++ src/app/blogs/[page]/page.tsx
@@ -4,31 +4,24 @@ import React from "react";
 
 async function getpost(id: string) {
   const res = await getData("/posts/" + id);
-  
   return res.data;
 }
 const page = async ({ params }: { params: { page: string } }) => {
-  const data = await getpost(params.page)
-  let post;
-  if (data) {
-     post = <div>{data.title}</div>
-  } else {
-     post = <div>loading</div>
-  }
-  return <>
-  <div className="bg-white">
-      <Navigation />
-      <div className="mx-auto p-3 max-w-7xl min-h-screen">
-        <h2 className="text-2xl text-center font-semibold">{data.title}</h2>
-        <div className="my-10 prose mx-auto flex justify-center" >
-          {/* {data.body} */}
-          <div dangerouslySetInnerHTML={{__html: data.body}}></div>
+  const data = await getpost(params.page);
+  return (
+    <>
+      <div className="bg-white">
+        <Navigation />
+        <div className="mx-auto p-3 max-w-7xl min-h-screen">
+          <h2 className="text-2xl text-center font-semibold">{data.title}</h2>
+          <div className="my-10 prose mx-auto flex justify-center">
+            {/* {data.body} */}
+            <div dangerouslySetInnerHTML={{ __html: data.body }}></div>
+          </div>
         </div>
-        
       </div>
-    </div>
- 
-  </>;
+    </>
+  );
 };
 
 export default page;
