360544ef75e4d0936d8679c17d6e51bfe75706fc
cecep31 <cecepjanuardi31@gmail.com>
2023-08-08 13:38:08 +0700

    increase the seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="bg-gray-50">
+      <head>
+        <meta name="robots" content="all" />
+      </head>
       <body className={inter.className}>
         {children}
         <Toaster />
