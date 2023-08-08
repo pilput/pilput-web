39c01dd6df35f576a06113add77ab124c536e76d
cecep31 <cecepjanuardi31@gmail.com>
2023-08-08 15:50:51 +0700

    update post

--- src/components/post/Postlist.tsx
+++ src/components/post/Postlist.tsx
@@ -13,12 +13,15 @@ interface PostlistProps {
 }
 
 const Postlist: React.FC<PostlistProps> = ({ post }) => {
+  const plaintext = post.body.replace(/(<([^>]+)>)/gi, "");
   return (
     <div className="w-full mt-4 px-5 py-5 bg-gray-50 border text-gray-600 shadow-md rounded-lg">
       <div className="">
-        <h2 className="font-bold capitalize">{post.title}</h2>
-        <p>
-          <WordLimit text={post.body} limit={50} />
+        <Link href={"/blogs/" + post.slug}>
+          <Button variant={"link"} className="font-bold capitalize text-xl">{post.title}</Button>
+        </Link>
+        <p className="px-4">
+          <WordLimit text={plaintext} limit={50} />
         </p>
         <div className="flex justify-end">
           <Link href={"/blogs/" + post.slug} className="btn">
