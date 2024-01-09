df0eb73a5214ec601edab46893419dd3fb688015
cecep31 <cecepjanuardi31@gmail.com>
2024-01-09 23:49:54 +0700

    show picture

--- src/app/blogs/[slug]/page.tsx
+++ src/app/blogs/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import Navigation from "@/components/header/Navbar";
 import Comment from "@/components/post/Comment";
 import { axiosIntence } from "@/utils/fetch";
+import Image from "next/image";
 import { notFound } from "next/navigation";
 import React from "react";
 
@@ -19,15 +20,15 @@ const page = async ({ params }: { params: { slug: string } }) => {
     <>
       <Navigation />
       <div className="mx-auto p-3 min-h-screen prose prose-sm sm:prose lg:prose-lg">
+        {post.photo_url && <Image className="mx-auto" src={post.photo_url} alt={post.photo_url} width={300} height={200} />}
+
         <div className="border-b-2">
           <div className="text-5xl text-gray-950 mx-auto my-7 font-bold">
             {post.title}
           </div>
         </div>
         <div className="my-10 mx-auto flex justify-center ">
-          <div
-            dangerouslySetInnerHTML={{ __html: post.body }}
-          ></div>
+          <div dangerouslySetInnerHTML={{ __html: post.body }}></div>
         </div>
         <Comment post_id={post.id} />
       </div>
