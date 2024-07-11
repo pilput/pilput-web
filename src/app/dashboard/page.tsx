aa3644a3605df5bb4a533bb5c5a9baf0648c9e40
cecep31 <cecepjanuardi31@gmail.com>
2024-07-11 15:44:19 +0700

    try chart ui hadcd

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,11 @@
 import PostsChart from "@/components/dashboard/PostChart"
+import LikeChart from "@/components/dashboard/likeChat"
 
 function page() {
   return (
-    <div className="flex">
+    <div className="flex gap-4">
         <PostsChart />
+        <LikeChart />
     </div>
   )
 }
