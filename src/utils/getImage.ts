48d5ca653fbcfe835d270c61e7bf4b68e22399fb
cecep31 <cecepjanuardi31@gmail.com>
2024-08-28 22:28:34 +0700

    fix bug

--- src/utils/getImage.ts
+++ src/utils/getImage.ts
@@ -4,13 +4,15 @@ function isFullUrl(url: string): boolean {
     return url.startsWith('http://') || url.startsWith('https://');
 }
 export function getProfilePicture(url: string) {
-    if (isFullUrl(url)) {
+    url = url ?? '';
+    if (isFullUrl(url ?? '')) {
         return url;
     } else {
         return storagebaseurl + url
     }
 }
 export function getUrlImage(path: string): string {
+    path = path ?? '';
     if (isFullUrl(path)) {
         return path;
     } else {
