e267a7d45e9e75dd7a63633e71261fa415e07f00
cecep <cecepjanuardi@proton.me>
2025-11-18 22:13:01 +0700

    fix: update default API URLs in Config class for better functionality

--- src/utils/getCofig.ts
+++ src/utils/getCofig.ts
@@ -1,9 +1,9 @@
 export class Config {
-    public static apibaseurl = process.env.NEXT_PUBLIC_API_URL || "";
-    public static apibaseurl2 = process.env.NEXT_PUBLIC_API_URL_2 || "";
-    public static dashbaseurl = process.env.NEXT_PUBLIC_DASH_URL || "";
-    public static wsbaseurl = process.env.NEXT_PUBLIC_WS_URL || "";
-    public static storagebaseurl = process.env.NEXT_PUBLIC_STORAGE_URL || "";
-    public static mainbaseurl = process.env.NEXT_PUBLIC_MAIN_URL || "";
-    public static maindoman = process.env.NEXT_PUBLIC_DOMAIN || "";
+    public static apibaseurl = process.env.NEXT_PUBLIC_API_URL || "https://echo.pilput.me";
+    public static apibaseurl2 = process.env.NEXT_PUBLIC_API_URL_2 || "https://api.pilput.me";
+    public static dashbaseurl = process.env.NEXT_PUBLIC_DASH_URL || "https://dashboard.pilput.me";
+    public static wsbaseurl = process.env.NEXT_PUBLIC_WS_URL || "https://api.pilput.me";
+    public static storagebaseurl = process.env.NEXT_PUBLIC_STORAGE_URL || "https://d42zd71vraxqs.cloudfront.net";
+    public static mainbaseurl = process.env.NEXT_PUBLIC_MAIN_URL || "https://pilput.me";
+    public static maindoman = process.env.NEXT_PUBLIC_DOMAIN || "pilput.me";
 }
