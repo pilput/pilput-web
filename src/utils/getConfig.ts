93115ffbe15e22489cf1f527133cf3599196ff13
cecep <cecepjanuardi@proton.me>
2026-04-04 21:38:36 +0700

    fix(config): update API base URL for consistency across the application
    - Changed the API base URL in getConfig.ts to point to the new endpoint at fiber.pilput.net.
    - Updated the fetch call in sitemap.ts to use the updated base URL, ensuring correct API interactions.

--- src/utils/getConfig.ts
+++ src/utils/getConfig.ts
@@ -9,7 +9,7 @@ export interface AppConfig {
 }
 
 export const Config: AppConfig = {
-  apibaseurl: process.env.NEXT_PUBLIC_API_URL || "https://axum-pilput.up.railway.app",
+  apibaseurl: process.env.NEXT_PUBLIC_API_URL || "https://fiber.pilput.net",
   apibaseurl2: process.env.NEXT_PUBLIC_API_URL_2 || "https://api.pilput.net",
   apibaseurl3: process.env.NEXT_PUBLIC_API_URL_3 || "https://hono.pilput.net",
   wsbaseurl: process.env.NEXT_PUBLIC_WS_URL || "https://api.pilput.net",
